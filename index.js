3ef2fc7ec70008f990baf247ad2a9e08e4cd789a
eviirus <evaldas.zuralovic@gmail.com>
2024-12-10 15:44:50 +0200

    stage

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { infoSheet } from './infoSheet.js';
 import { steps } from './steps.js';
 
@@ -9,15 +8,3 @@ document.addEventListener('DOMContentLoaded', () => {
 
   guide.initialize();
 });
-=======
-import { infoSheet } from './infoSheet.js';
-import { steps } from './steps.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const guide = new TravelGuide(steps, infoSheet);
-  guide.stepsManager = new StepManager(guide);
-  guide.infoSheetManager = new InfoSheetManager(guide);
-
-  guide.initialize();
-});
->>>>>>> 620af92a668dd50f4adb8696cb7258668d8217b0
--- infoSheetManager.js
+++ infoSheetManager.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class InfoSheetManager {
     constructor(guide) {
       this.guide = guide;
@@ -17,24 +16,4 @@ class InfoSheetManager {
       }
     }
   }
-=======
-class InfoSheetManager {
-    constructor(guide) {
-      this.guide = guide;
-    }
-  
-    displayInfoSheet(choice) {
-      const infoChange = document.querySelectorAll('.info-changing');
-      const selectedInfo = this.guide.infoSheet.find(item => item.element === choice);
-  
-      if (selectedInfo) {
-        selectedInfo.content.forEach((content, index) => {
-          infoChange[index].textContent = content || " ";
-        });
-      } else {
-        infoChange.forEach(content => content.textContent = " ");
-      }
-    }
-  }
->>>>>>> 620af92a668dd50f4adb8696cb7258668d8217b0
   
